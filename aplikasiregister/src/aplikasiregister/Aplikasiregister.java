3f9e6291d84065c655b84abfd0640f47e47509c9
Muhammad Ihsan <55564001+ihsan946@users.noreply.github.com>
2019-12-17 22:42:16 +0700

    Add files via upload

---
+++ aplikasiregister/src/aplikasiregister/Aplikasiregister.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplikasiregister;
+
+/**
+ *
+ * @author Muhammad Ihsan
+ */
+public class Aplikasiregister {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        menuregister register = new menuregister();
+        register.setVisible(true);
+        
+    }
+    
+}
